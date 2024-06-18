49a062467ef54cdb4190adc41fea4476eb81aa18
Abel Roinsard <104033433+kaitomomotta@users.noreply.github.com>
2024-06-18 16:13:34 +0200

    ambiance + cut trees

--- Assets/Scripts/SC_FPSController.cs
+++ Assets/Scripts/SC_FPSController.cs
@@ -41,6 +41,7 @@ public class SC_FPSController : MonoBehaviour
     public float lookXLimit = 45.0f;
 
     CharacterController characterController;
+    public Light flashlight;
     Vector3 moveDirection = Vector3.zero;
     float rotationX = 0;
 
@@ -90,6 +91,7 @@ public class SC_FPSController : MonoBehaviour
             rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
             rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            flashlight.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
 
