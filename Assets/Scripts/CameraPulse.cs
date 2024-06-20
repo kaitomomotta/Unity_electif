1fb7dd67a2fcf1cef77c65a3a4f763e167b31104
Cancan-68 <simonx368@gmail.com>
2024-06-20 17:34:28 +0200

    scene commit

--- Assets/Scripts/CameraPulse.cs
+++ Assets/Scripts/CameraPulse.cs
@@ -8,18 +8,18 @@ public class CameraPulse : MonoBehaviour
     GameObject slender;
     PostProcessVolume m_Volume;
     Vignette m_Vignette;
-
+    ChromaticAberration m_ChromaticAberration;
     // Start is called before the first frame update
     void Start()
     {
         slender = GameObject.Find("Slender");
         m_Vignette = GetComponent<Vignette>();
-
+        m_ChromaticAberration = GetComponent<ChromaticAberration>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        m_Vignette.intensity.value = Mathf.Sin(Time.realtimeSinceStartup);
     }
 }
